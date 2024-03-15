45f7cce52c8482751bc228d1b12b6329dc3218a0
Jerome <jerome.neupert@epitech.eu>
2024-03-15 23:52:10 +0100

    init

---
+++ src/HomePage/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import  "./Home.css";
+
+
+
+const Home = () => {
+  return (
+    <main className='container'>
+      <div className='projectList'>
+      <h1>Premières énigmes</h1>
+      </div>
+    </main>
+  )
+}
+
+export default Home
